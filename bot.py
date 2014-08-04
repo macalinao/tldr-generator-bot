22fc1a71d42b2797d70315f94ece65ca5e647359
Ian Macalinao <me@ian.pw>
2014-08-04 15:38:45 -0500

    formatting
    Signed-off-by: Ian Macalinao <me@ian.pw>

--- bot.py
+++ bot.py
@@ -34,16 +34,15 @@ for comment in praw.helpers.comment_stream(reddit, '+'.join(subreddits)):
     match = re.search(regex, comment.body, re.IGNORECASE)
     if match and not(already.get(cid)):
         try:
-            summary = ' '.join(Summarize(comment.submission.title, comment.body))
+            summary = ' '.join(
+                Summarize(comment.submission.title, comment.body))
             comment.reply("**TLDR:** " + summary)
             already.set(cid, "True")
-        # If you are banned from a subreddit, reddit throws a 403 instead of a
-        # helpful message :/
+
         except HTTPError as err:
             print("Probably banned from /r/" +
                   str(comment.subreddit), file=sys.stderr)
-        # This one is pretty rare, since PRAW controls the rate automatically, but
-        # just in case
+
         except praw.errors.RateLimitExceeded as err:
             print("Rate Limit Exceeded:\n" + str(err), file=sys.stderr)
             time.sleep(err.sleep_time)
